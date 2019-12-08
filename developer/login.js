62bcd813e038ac1bfe14c78c701956e98f2349e0
Levminer <33373714+Levminer@users.noreply.github.com>
2019-12-08 14:20:52 +0100

    .

---
+++ developer/login.js
@@ -0,0 +1,30 @@
+
+// adatok
+let name = "admin"
+let pass = "01"
+var ok = 0
+let i = 1
+
+// bejelentkezés
+do {
+
+    let name1 = prompt("Név:")
+    let pass1 = prompt("Jelszó:")
+
+    if (name==name1) {
+        
+        if (pass==pass1) {
+            
+            var ok = 1
+            alert("Üdv!")
+            
+        } else {
+            
+            alert("Hibás név vagy jelszó!")
+
+        }
+
+    } else {
+        alert("Hibás név vagy jelszó!")
+    }
+} while (i>ok);
