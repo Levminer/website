68a68d0f05df7e445cb6127ca8ec02fd7eb3fde2
Levminer <33373714+Levminer@users.noreply.github.com>
2020-09-14 20:32:58 +0200

    2020.09.Update

--- src/js/index.js
+++ src/js/index.js
@@ -1,137 +1,141 @@
 if (screen.width <= 700) {
-    window.location = "https://www.levminer.me/projects.html"
+	window.location = "https://www.levminer.com/projects.html"
 }
 
 const oneElement = document.querySelector("#ityped")
 
 const text = window.ityped.init(oneElement, {
-    showCursor: false,
-    typeSpeed: 100,
-    strings: [
-        "Welcome",
-        "Check out my Twitter",
-        "Tesla Calculator",
-        "Created by: Levminer",
-        "Made with love by: Levminer",
-        "Netflix Statistics",
-        "Check out my GitHub",
-        "Hi",
-    ],
+	showCursor: false,
+	typeSpeed: 100,
+	strings: [
+		"Welcome",
+		"Check Out My Twitter",
+		"Creator Of Tesla Calculator",
+		"<3",
+		"Creator Of Netflix Statistics",
+		"Check Out My GitHub",
+		"Hi",
+		"Creator Of TYPR",
+		"Feel Free To Email Me",
+		"Good To See You",
+		"Creator of ASP",
+		"Feel free to Check out My Projects",
+	],
 })
 
 particlesJS("particles-js", {
-    particles: {
-        number: {
-            value: 60,
-            density: {
-                enable: true,
-                value_area: 800,
-            },
-        },
-        color: {
-            value: "#ffffff",
-        },
-        shape: {
-            type: "circle",
-            stroke: {
-                width: 0,
-                color: "#000000",
-            },
-            polygon: {
-                nb_sides: 5,
-            },
-            image: {
-                src: "img/github.svg",
-                width: 100,
-                height: 100,
-            },
-        },
-        opacity: {
-            value: 0.5,
-            random: false,
-            anim: {
-                enable: false,
-                speed: 1,
-                opacity_min: 0.1,
-                sync: false,
-            },
-        },
-        size: {
-            value: 5,
-            random: true,
-            anim: {
-                enable: false,
-                speed: 40,
-                size_min: 0.1,
-                sync: false,
-            },
-        },
-        line_linked: {
-            enable: true,
-            distance: 150,
-            color: "#ffffff",
-            opacity: 0.4,
-            width: 1,
-        },
-        move: {
-            enable: true,
-            speed: 6,
-            direction: "none",
-            random: false,
-            straight: false,
-            out_mode: "out",
-            attract: {
-                enable: false,
-                rotateX: 600,
-                rotateY: 1200,
-            },
-        },
-    },
-    interactivity: {
-        detect_on: "canvas",
-        events: {
-            onhover: {
-                enable: true,
-                mode: "repulse",
-            },
-            onclick: {
-                enable: true,
-                mode: "push",
-            },
-            resize: true,
-        },
-        modes: {
-            grab: {
-                distance: 400,
-                line_linked: {
-                    opacity: 1,
-                },
-            },
-            bubble: {
-                distance: 400,
-                size: 40,
-                duration: 2,
-                opacity: 8,
-                speed: 3,
-            },
-            repulse: {
-                distance: 200,
-            },
-            push: {
-                particles_nb: 4,
-            },
-            remove: {
-                particles_nb: 2,
-            },
-        },
-    },
-    retina_detect: true,
-    config_demo: {
-        hide_card: false,
-        background_color: "#b61924",
-        background_image: "",
-        background_position: "50% 50%",
-        background_repeat: "no-repeat",
-        background_size: "cover",
-    },
+	particles: {
+		number: {
+			value: 60,
+			density: {
+				enable: true,
+				value_area: 800,
+			},
+		},
+		color: {
+			value: "#ffffff",
+		},
+		shape: {
+			type: "circle",
+			stroke: {
+				width: 0,
+				color: "#000000",
+			},
+			polygon: {
+				nb_sides: 5,
+			},
+			image: {
+				src: "img/github.svg",
+				width: 100,
+				height: 100,
+			},
+		},
+		opacity: {
+			value: 0.5,
+			random: false,
+			anim: {
+				enable: false,
+				speed: 1,
+				opacity_min: 0.1,
+				sync: false,
+			},
+		},
+		size: {
+			value: 5,
+			random: true,
+			anim: {
+				enable: false,
+				speed: 40,
+				size_min: 0.1,
+				sync: false,
+			},
+		},
+		line_linked: {
+			enable: true,
+			distance: 150,
+			color: "#ffffff",
+			opacity: 0.4,
+			width: 1,
+		},
+		move: {
+			enable: true,
+			speed: 6,
+			direction: "none",
+			random: false,
+			straight: false,
+			out_mode: "out",
+			attract: {
+				enable: false,
+				rotateX: 600,
+				rotateY: 1200,
+			},
+		},
+	},
+	interactivity: {
+		detect_on: "canvas",
+		events: {
+			onhover: {
+				enable: true,
+				mode: "repulse",
+			},
+			onclick: {
+				enable: true,
+				mode: "push",
+			},
+			resize: true,
+		},
+		modes: {
+			grab: {
+				distance: 400,
+				line_linked: {
+					opacity: 1,
+				},
+			},
+			bubble: {
+				distance: 400,
+				size: 40,
+				duration: 2,
+				opacity: 8,
+				speed: 3,
+			},
+			repulse: {
+				distance: 200,
+			},
+			push: {
+				particles_nb: 4,
+			},
+			remove: {
+				particles_nb: 2,
+			},
+		},
+	},
+	retina_detect: true,
+	config_demo: {
+		hide_card: false,
+		background_color: "#b61924",
+		background_image: "",
+		background_position: "50% 50%",
+		background_repeat: "no-repeat",
+		background_size: "cover",
+	},
 })
