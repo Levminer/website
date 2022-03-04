195d781d83b6d4bac01af1bb3c399d1ae81e0480
Levminer <33373714+Levminer@users.noreply.github.com>
2022-03-04 15:47:33 +0100

    2022-03-04-15-45

---
+++ pages/policies/terms.jsx
@@ -0,0 +1,46 @@
+const Terms = () => {
+	return (
+		<div>
+			<h1 className="h1">Terms Of Service for Levminer and its services</h1>
+
+			<h2 className="h2">1. Terms</h2>
+
+			<h3 className="h3">By accessing this Website, accessible from www.levminer.com, you are agreeing to be bound by these Website Terms and Conditions of Use and agree that you are responsible for the agreement with any applicable local laws. If you disagree with any of these terms, you are prohibited from accessing this site. The materials contained in this Website are protected by copyright and trade mark law.</h3>
+
+			<h2 className="h2">2. Use License</h2>
+
+			<h3 className="h3">Permission is granted to temporarily download one copy of the materials on Levminer's Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not:</h3>
+
+			<h3 className="h3">
+				This will let Levminer to terminate upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated and you should destroy any downloaded materials in your possession whether it is printed or electronic format. modify or copy the materials; use the materials for any commercial purpose or for any public display; attempt to reverse engineer any software contained on Levminer's Website; remove any copyright or other proprietary notations
+				from the materials; or transferring the materials to another person or "mirror" the materials on any other server.
+			</h3>
+
+			<h2 className="h2">3. Disclaimer</h2>
+
+			<h3 className="h3">All the materials on Levminer’s Website are provided "as is". Levminer makes no warranties, may it be expressed or implied, therefore negates all other warranties. Furthermore, Levminer does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.</h3>
+
+			<h2 className="h2">4. Limitations</h2>
+
+			<h3 className="h3">Levminer or its suppliers will not be hold accountable for any damages that will arise with the use or inability to use the materials on Levminer’s Website, even if Levminer or an authorize representative of this Website has been notified, orally or written, of the possibility of such damage. Some jurisdiction does not allow limitations on implied warranties or limitations of liability for incidental damages, these limitations may not apply to you.</h3>
+
+			<h2 className="h2">5. Revisions and Errata</h2>
+
+			<h3 className="h3">The materials appearing on Levminer’s Website may include technical, typographical, or photographic errors. Levminer will not promise that any of the materials in this Website are accurate, complete, or current. Levminer may change the materials contained on its Website at any time without notice. Levminer does not make any commitment to update the materials.</h3>
+
+			<h2 className="h2">6. Links</h2>
+
+			<h3 className="h3">Levminer has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by Levminer of the site. The use of any linked website is at the user’s own risk.</h3>
+
+			<h2 className="h2">7. Site Terms of Use Modifications</h2>
+
+			<h3 className="h3">Levminer may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.</h3>
+
+			<h2 className="h2">8. Governing Law</h2>
+
+			<h3 className="h3">Any claim related to Levminer's Website shall be governed by the laws of Hungary without regards to its conflict of law provisions.</h3>
+		</div>
+	)
+}
+
+export default Terms
