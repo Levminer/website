55b4d7a553078506d61c40838f4b03b1eaf6f2e8
Lőrik Levente <33373714+Levminer@users.noreply.github.com>
2024-11-07 23:57:06 +0100

    New home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,60 +1,81 @@
 import Link from "next/link"
 
 const Home = () => {
+	let i = 0
+
 	return (
 		<>
-			<div className="bg relative">
-				<div className="mt-72 text-center">
-					<h1 className="text-6xl font-bold sm:text-8xl">Levminer</h1>
+			<div className="relative min-h-screen font-mono">
+				<div className="mx-1 mt-16 rounded-2xl bg-black p-10 text-left text-lg sm:mx-20 sm:text-2xl">
+					<div className="flex gap-1">
+						<MarkdownSymbol symbol="#" line={i++} />
+						<h1>Lőrik Levente</h1>
+					</div>
 
-					<div className="mt-5 flex items-center justify-center gap-5">
-						<a
-							href="https://twitter.com/levminer92"
-							rel="noopener noreferrer"
-							target="_blank"
-							className="duration-200 ease-in hover:text-gray-300"
-							aria-label="Twitter"
-						>
-							<svg role="img" className="h-24 w-24" fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-								<title>X Logo</title>
-								<path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
-							</svg>
-						</a>
+					<div className="flex gap-1">
+						<MarkdownSymbol symbol="*" line={i++} />
+						<h1>Freelance Software Engineer</h1>
+					</div>
 
-						<a
-							href="mailto:levminer@levminer.com"
-							rel="noopener noreferrer"
-							target="_blank"
-							className="duration-200 ease-in hover:text-gray-300"
-							aria-label="Email"
-						>
-							<svg xmlns="http://www.w3.org/2000/svg" className="h-24 w-24" fill="currentColor" viewBox="0 0 512 512">
-								<title>Email Logo</title>
-								<path d="M424 80H88a56.06 56.06 0 00-56 56v240a56.06 56.06 0 0056 56h336a56.06 56.06 0 0056-56V136a56.06 56.06 0 00-56-56zm-14.18 92.63l-144 112a16 16 0 01-19.64 0l-144-112a16 16 0 1119.64-25.26L256 251.73l134.18-104.36a16 16 0 0119.64 25.26z" />
-							</svg>
-						</a>
+					<div className="flex gap-1">
+						<MarkdownSymbol symbol="" line={i++} />
+						<br />
+					</div>
 
+					<div className="flex gap-1">
+						<MarkdownSymbol symbol="##" line={i++} />
+						<h2>Open-source stuff:</h2>
+					</div>
+
+					<div className="flex gap-1">
+						<MarkdownSymbol symbol="*" line={i++} />
+						<Link className="font-semibold text-blue-500 duration-200 ease-in hover:underline" href="/projects">
+							[Projects](/projects)
+						</Link>
+					</div>
+
+					<div className="flex gap-1">
+						<MarkdownSymbol symbol="" line={i++} />
+						<br />
+					</div>
+
+					<div className="flex gap-1">
+						<MarkdownSymbol symbol="##" line={i++} />
+						<h2>Socials:</h2>
+					</div>
+
+					<div className="flex gap-1">
+						<MarkdownSymbol symbol="*" line={i++} />
 						<a
 							href="https://github.com/levminer"
-							rel="noopener noreferrer"
+							className="font-semibold text-blue-500 duration-200 ease-in hover:underline"
 							target="_blank"
-							className="duration-200 ease-in hover:text-gray-300"
-							aria-label="GitHub"
+							rel="noopener noreferrer"
 						>
-							<svg role="img" viewBox="0 0 24 24" className="h-24 w-24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-								<title>GitHub Logo</title>
-								<path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
-							</svg>
+							[GitHub](github.com/levminer)
+						</a>
+					</div>
+					<div className="flex gap-1">
+						<MarkdownSymbol symbol="*" line={i++} />
+						<a
+							href="https://twitter.com/levminer92"
+							className="font-semibold text-blue-500 duration-200 ease-in hover:underline"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							[Twitter](twitter.com/levminer92)
 						</a>
 					</div>
 
-					<div className="mx-auto mt-10 flex w-max justify-center">
-						<Link className="flex flex-row text-4xl duration-200 ease-in hover:text-gray-300" href="/projects">
-							Projects
-							<svg xmlns="http://www.w3.org/2000/svg" className="relative top-1.5 h-8 w-8" viewBox="0 0 20 20" fill="currentColor">
-								<path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd" />
-							</svg>
-						</Link>
+					<div className="flex gap-1">
+						<MarkdownSymbol symbol="*" line={i++} />
+						<a href="mailto:contact@levminer.com" className="font-semibold text-blue-500 duration-200 ease-in hover:underline">
+							[Email](contact@levminer.com)
+						</a>
+					</div>
+
+					<div className="flex gap-1">
+						<MarkdownSymbol symbol="" line={i++} />
 					</div>
 				</div>
 			</div>
@@ -64,3 +85,11 @@ const Home = () => {
 
 export default Home
 
+const MarkdownSymbol = ({ symbol, line }) => {
+	return (
+		<div className="flex">
+			<span className="w-14 font-bold text-gray-500">{line}</span>
+			<span className="font-bold text-gray-400">{symbol}</span>
+		</div>
+	)
+}
